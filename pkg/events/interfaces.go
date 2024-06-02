25a0696c34085b5a46932aea14ffc3d822d285e6
silva4dev <web4dev.lucas@outlook.com>
2024-06-02 08:14:48 -0300

    feat: change interfaces

--- pkg/events/interfaces.go
+++ pkg/events/interfaces.go
@@ -1,21 +1,25 @@
 package events
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 type EventInterface interface {
 	GetName() string
 	GetDateTime() time.Time
 	GetPayload() interface{}
+	SetPayload(payload interface{})
 }
 
-type EventHanlderInterface interface {
-	Handle(event EventInterface)
+type EventHandlerInterface interface {
+	Handle(event EventInterface, wg *sync.WaitGroup)
 }
 
 type EventDispatcherInterface interface {
-	Register(eventName string, handler EventHanlderInterface) error
+	Register(eventName string, handler EventHandlerInterface) error
 	Dispatch(event EventInterface) error
-	Remove(eventName string, handler EventHanlderInterface) error
-	Has(eventName string, handler EventHanlderInterface) bool
-	Clear() error
+	Remove(eventName string, handler EventHandlerInterface) error
+	Has(eventName string, handler EventHandlerInterface) bool
+	Clear()
 }
