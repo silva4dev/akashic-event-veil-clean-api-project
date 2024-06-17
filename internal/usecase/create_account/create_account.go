001ad78ccd0121daf9c3ba1af7369b7c8ddb35f3
silva4dev <web4dev.lucas@outlook.com>
2024-06-16 22:37:05 -0300

    feat: add create account use case

---
+++ internal/usecase/create_account/create_account.go
@@ -0,0 +1,44 @@
+package createaccount
+
+import (
+	"github/silva4dev/golang-event-driven-arch-project/internal/entity"
+	"github/silva4dev/golang-event-driven-arch-project/internal/gateway"
+)
+
+type CreateAccountInputDTO struct {
+	ClientID string
+}
+
+type CreateAccountOutputDTO struct {
+	ID string
+}
+
+type CreateAccountUseCase struct {
+	AccountGateway gateway.AccountGateway
+	ClientGateway  gateway.ClientGateway
+}
+
+func NewCreateAccountUseCase(accountGateway gateway.AccountGateway, clientGateway gateway.ClientGateway) *CreateAccountUseCase {
+	return &CreateAccountUseCase{
+		AccountGateway: accountGateway,
+		ClientGateway:  clientGateway,
+	}
+}
+
+func (uc *CreateAccountUseCase) Execute(input CreateAccountInputDTO) (*CreateAccountOutputDTO, error) {
+	client, err := uc.ClientGateway.Get(input.ClientID)
+	if err != nil {
+		return nil, err
+	}
+
+	account := entity.NewAccount(client)
+	err = uc.AccountGateway.Save(account)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &CreateAccountOutputDTO{
+		ID: account.ID,
+	}, nil
+}
