5e8f2e28ed0c3b64db18fc4da438b2c3d1af35e9
silva4dev <web4dev.lucas@outlook.com>
2024-06-16 22:37:19 -0300

    test: add create client use case

---
+++ internal/usecase/create_client/create_client_test.go
@@ -0,0 +1,29 @@
+package createclient
+
+import (
+	"github/silva4dev/golang-event-driven-arch-project/internal/gateway/mocks"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestCreateClientUseCase_Execute(t *testing.T) {
+	m := mocks.NewClientGatewayMock()
+	m.On("Save", mock.Anything).Return(nil)
+	uc := NewCreateClientUseCase(m)
+
+	input := &CreateClientInputDTO{
+		Name:  "John Doe",
+		Email: "john@doe.com",
+	}
+
+	output, err := uc.Execute(input)
+	assert.Nil(t, err)
+	assert.NotNil(t, output)
+	assert.NotEmpty(t, output.ID)
+	assert.Equal(t, "John Doe", output.Name)
+	assert.Equal(t, "john@doe.com", output.Email)
+	m.AssertExpectations(t)
+	m.AssertNumberOfCalls(t, "Save", 1)
+}
