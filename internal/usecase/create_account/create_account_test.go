cbd7a769eafaa55fccdc4c6c2fbf1f54fdfb6f08
silva4dev <web4dev.lucas@outlook.com>
2024-06-16 22:36:55 -0300

    test: add create account use case

---
+++ internal/usecase/create_account/create_account_test.go
@@ -0,0 +1,32 @@
+package createaccount
+
+import (
+	"github/silva4dev/golang-event-driven-arch-project/internal/entity"
+	"github/silva4dev/golang-event-driven-arch-project/internal/gateway/mocks"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestCreateAccountUseCase_Execute(t *testing.T) {
+	client, _ := entity.NewClient("John Doe", "john@doe.com")
+	clientGatewayMock := mocks.NewClientGatewayMock()
+	clientGatewayMock.On("Get", client.ID).Return(client, nil)
+
+	accountGatewayMock := mocks.NewAccountGatewayMock()
+	accountGatewayMock.On("Save", mock.Anything).Return(nil)
+
+	uc := NewCreateAccountUseCase(accountGatewayMock, clientGatewayMock)
+	inputDto := CreateAccountInputDTO{
+		ClientID: client.ID,
+	}
+
+	output, err := uc.Execute(inputDto)
+	assert.Nil(t, err)
+	assert.NotNil(t, output.ID)
+	clientGatewayMock.AssertExpectations(t)
+	accountGatewayMock.AssertExpectations(t)
+	clientGatewayMock.AssertNumberOfCalls(t, "Get", 1)
+	accountGatewayMock.AssertNumberOfCalls(t, "Save", 1)
+}
