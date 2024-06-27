3706fa269cbc11581e13888f7bf741cb5e0f16e2
silva4dev <web4dev.lucas@outlook.com>
2024-06-26 22:09:27 -0300

    test: add transaction entity

--- internal/entity/transaction_test.go
+++ internal/entity/transaction_test.go
@@ -7,12 +7,14 @@ import (
 )
 
 func TestCreateTransaction(t *testing.T) {
-	client1, _ := NewClient("John Doe", "j@j.com")
+	client1, _ := NewClient("John Doe", "j@j")
 	account1 := NewAccount(client1)
-	client2, _ := NewClient("Jane Doe", "j@j2.com")
+	client2, _ := NewClient("John Doe 2", "j@j2")
 	account2 := NewAccount(client2)
+
 	account1.Credit(1000)
 	account2.Credit(1000)
+
 	transaction, err := NewTransaction(account1, account2, 100)
 	assert.Nil(t, err)
 	assert.NotNil(t, transaction)
@@ -20,13 +22,15 @@ func TestCreateTransaction(t *testing.T) {
 	assert.Equal(t, 900.0, account1.Balance)
 }
 
-func TestCreateTransactionWithInsufficientBalance(t *testing.T) {
-	client1, _ := NewClient("John Doe", "j@j.com")
+func TestCreateTransactionWithInsuficientBalance(t *testing.T) {
+	client1, _ := NewClient("John Doe", "j@j")
 	account1 := NewAccount(client1)
-	client2, _ := NewClient("Jane Doe", "j@j2.com")
+	client2, _ := NewClient("John Doe 2", "j@j2")
 	account2 := NewAccount(client2)
+
 	account1.Credit(1000)
 	account2.Credit(1000)
+
 	transaction, err := NewTransaction(account1, account2, 2000)
 	assert.NotNil(t, err)
 	assert.Error(t, err, "insufficient funds")
