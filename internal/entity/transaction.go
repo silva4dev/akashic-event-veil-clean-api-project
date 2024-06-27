fd177c336ac349f9f7150bfcb16df3f2b673b656
silva4dev <web4dev.lucas@outlook.com>
2024-06-26 22:09:35 -0300

    feat: add transaction entity

--- internal/entity/transaction.go
+++ internal/entity/transaction.go
@@ -8,11 +8,13 @@ import (
 )
 
 type Transaction struct {
-	ID          string
-	AccountFrom *Account
-	AccountTo   *Account
-	Amount      float64
-	CreatedAt   time.Time
+	ID            string
+	AccountFrom   *Account `gorm:"foreignKey:AccountFromID"`
+	AccountFromID string
+	AccountTo     *Account `gorm:"foreignKey:AccountToID"`
+	AccountToID   string
+	Amount        float64
+	CreatedAt     time.Time
 }
 
 func NewTransaction(accountFrom *Account, accountTo *Account, amount float64) (*Transaction, error) {
@@ -38,7 +40,7 @@ func (t *Transaction) Commit() {
 
 func (t *Transaction) Validate() error {
 	if t.Amount <= 0 {
-		return errors.New("amount must be greater than 0")
+		return errors.New("amount must be greater than zero")
 	}
 	if t.AccountFrom.Balance < t.Amount {
 		return errors.New("insufficient funds")
