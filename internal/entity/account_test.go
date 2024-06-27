d3e58431281dcd86566a2ffa3b56c756f5476259
silva4dev <web4dev.lucas@outlook.com>
2024-06-26 22:08:30 -0300

    test: add account entity

--- internal/entity/account_test.go
+++ internal/entity/account_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestCreateAccount(t *testing.T) {
-	client, _ := NewClient("John Doe", "j@j.com")
+	client, _ := NewClient("John Doe", "j@j")
 	account := NewAccount(client)
 	assert.NotNil(t, account)
 	assert.Equal(t, client.ID, account.Client.ID)
@@ -19,14 +19,14 @@ func TestCreateAccountWithNilClient(t *testing.T) {
 }
 
 func TestCreditAccount(t *testing.T) {
-	client, _ := NewClient("John Doe", "j@j.com")
+	client, _ := NewClient("John Doe", "j@j")
 	account := NewAccount(client)
 	account.Credit(100)
 	assert.Equal(t, float64(100), account.Balance)
 }
 
 func TestDebitAccount(t *testing.T) {
-	client, _ := NewClient("John Doe", "j@j.com")
+	client, _ := NewClient("John Doe", "j@j")
 	account := NewAccount(client)
 	account.Credit(100)
 	account.Debit(50)
