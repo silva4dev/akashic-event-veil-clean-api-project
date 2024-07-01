a0a0247041f4147ebb0544b35a3758176bee6e4d
silva4dev <web4dev.lucas@outlook.com>
2024-07-01 03:06:43 -0300

    feat: change account gateway

--- internal/gateway/account.go
+++ internal/gateway/account.go
@@ -5,4 +5,5 @@ import "github.com.br/silva4dev/golang-event-driven-arch-project/internal/entity
 type AccountGateway interface {
 	Save(account *entity.Account) error
 	FindByID(id string) (*entity.Account, error)
+	UpdateBalance(account *entity.Account) error
 }
